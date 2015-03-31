0efe0938a8952576243ba084298a9d1e9697e0e3
14214290028 <14214290028@OLAB3-D18.academico.iesb.br>
2015-03-30 22:41:05 -0300

    Primeiro Dia.

---
+++ OlaMundo/src/trabalhoJava/iesb/Produtos.java
@@ -0,0 +1,85 @@
+package trabalhoJava.iesb;
+
+import java.util.Date;
+
+public class Produtos {
+	private int id;
+	private String nome;
+	private String marca;
+	private String genero;
+	private double valorDeCusto;
+	private double margemDeLucro;
+	private Date dataDeFabricacao;
+	private Date validade;
+	private String categoria;
+	private int unidades;
+	private double quantidade;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public double getValorDeCusto() {
+		return valorDeCusto;
+	}
+	public void setValorDeCusto(double valorDeCusto) {
+		this.valorDeCusto = valorDeCusto;
+	}
+	public double getMargemDeLucro() {
+		return margemDeLucro;
+	}
+	public void setMargemDeLucro(double margemDeLucro) {
+		this.margemDeLucro = margemDeLucro;
+	}
+	public Date getDataDeFabricacao() {
+		return dataDeFabricacao;
+	}
+	public void setDataDeFabricacao(Date dataDeFabricacao) {
+		this.dataDeFabricacao = dataDeFabricacao;
+	}
+	public Date getValidade() {
+		return validade;
+	}
+	public void setValidade(Date validade) {
+		this.validade = validade;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public int getUnidades() {
+		return unidades;
+	}
+	public void setUnidades(int unidades) {
+		this.unidades = unidades;
+	}
+	public double getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(double quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+		
+}
