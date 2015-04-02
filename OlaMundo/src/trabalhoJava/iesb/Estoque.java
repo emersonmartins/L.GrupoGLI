3b9e76006f075664f7ca476173879dc2f6d36434
ilderlan <ilderlan@Trabalho-530U3C-530U4C-532U3C>
2015-04-01 21:30:57 -0300

    Aula Engenharia...Melhorando!

---
+++ OlaMundo/src/trabalhoJava/iesb/Estoque.java
@@ -0,0 +1,16 @@
+package trabalhoJava.iesb;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Estoque {
+		
+	public static void main(String[] args) {
+		Locale localBR = new Locale("pt","BR");
+		Calendar dataAtual = Calendar.getInstance();
+		DateFormat formatarData = DateFormat.getDateInstance(DateFormat.FULL,localBR);
+		
+				
+	}
+}
